f324227c4ca91cf4a78d995c6eac1a79f8ad0171
andrew <andrewxhill@gmail.com>
2019-04-30 12:40:57 -0700

    code

---
+++ codeyaml/plugin.py
@@ -0,0 +1,35 @@
+import yaml
+import os
+from mkdocs.plugins import BasePlugin
+
+from jinja2 import Template
+
+
+CONFIG_KEYS = [
+    'site_name',
+    'site_author',
+    'site_url',
+    'repo_url',
+    'repo_name'
+]
+
+
+class CodeYamlPlugin(BasePlugin):
+    """
+    Inject certain config variables into the markdown. Include secondary yml files
+    """
+    def on_page_markdown(self, markdown, config, **kwargs):
+        context = {key: config.get(key) for key in CONFIG_KEYS if key in config}
+        context.update(config.get('extra', {}))
+        extra = config.get('extra')
+        
+        clients = extra.get('clients')
+        for client in clients:
+            path = os.path.dirname(os.path.abspath(__file__))
+            yml = os.path.join(os.getcwd(), '{}.yml'.format(client.get('yaml')))
+            with open(yml, 'r') as f:
+                contents = yaml.safe_load(f) or {}
+                context.update(contents)
+                extra.update(contents)
+        md_template = Template(markdown)
+        return md_template.render(**extra)
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='mkdocs-codeyaml-plugin',
+    version='0.0.12',
+    description='A MkDocs plugin that injects the mkdocs.yml extra variables PLUS additional YML files into the markdown template',
+    long_description=read('README.md'),
+    keywords='mkdocs python markdown extra values PLUS extra yml',
+    url='https://github.com/textileio/mkdocs-codeyaml-plugin/',
+    author='textile',
+    author_email='contact@textile.io',
+    license='MIT',
+    python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    install_requires=[
+        'mkdocs>=0.17'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    entry_points={
+        'mkdocs.plugins': [
+            'codeyaml = codeyaml.plugin:CodeYamlPlugin'
+        ]
+    }
+)
